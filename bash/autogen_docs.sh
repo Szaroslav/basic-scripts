249ae38e25c36c34fee7dba927d8471bb8a7fc5b
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-05-04 23:15:23 +0200

    Read `README`s line by line

--- bash/autogen_docs.sh
+++ bash/autogen_docs.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
 OUTPUT_FN=output.md
-found_title=false
+title=""
+EX_PATTERN="[Ee]xercise ?[0-9]+\.[0-9]+"
+EX_README_PATTERN="$EX_PATTERN/README.md$"
 
-echo "" > $OUTPUT_FN
+printf "" > $OUTPUT_FN
 
-find test/autogen_docs/chapter01 -print0 | while IFS= read -r -d '' file
+find test/autogen_docs/chapter01 -print0 | sort -z | while IFS= read -r -d '' file
 do
-    if [ -d "$file" ]; then
-        if ! [ $found_title ] && [[ $file =~ exercise[0-9]+\.[0-9]+$ ]]; then
-            found_title=true
-            echo "$found_title"
+    echo "$file"
+    if [ ! -d $file ]; then
+        if [[ $file =~ $EX_README_PATTERN ]] && [[ $title != $(sed '1q;d' $file) ]]; then
+            title=$(sed '1q;d' $file)
+            echo -e "$title\n" >> $OUTPUT_FN
         fi
-        echo "$file"
-    elif [[ $file =~ README.md$ ]]; then
-        cat $file >> $OUTPUT_FN
-        printf "\n\n" >> $OUTPUT_FN
+        
+        if [[ $file =~ README.md$ ]]; then
+            write=false
+            while IFS= read -r line
+            do
+                if [[ $line =~ $EX_PATTERN ]]; then
+                    write=true
+                fi
+                if $write; then
+                    # echo $line
+                    echo $line >> $OUTPUT_FN
+                fi
+            done < $file
+        fi
     fi
 done
