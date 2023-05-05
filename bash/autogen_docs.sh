20160a2ca9d533affd084e2e9e912219c92babb4
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-05-05 22:45:06 +0200

    Create basic path resolving function

--- bash/autogen_docs.sh
+++ bash/autogen_docs.sh
@@ -12,16 +12,63 @@ parse_path () {
     echo "$1" | sed "s/$DOCS_PATTERN//"
 }
 
+# Resolve 2 relative paths
 resolve_path () {
-    PATTERN="chapter01\/(.*)"
-    echo "$1 $2"
+    if [[ $1 == "" ]]; then
+        echo "resolve_path: [Error] First argument missing"
+        return 1
+    fi
+    if [[ $2 == "" ]]; then
+        echo "resolve_path: [Error] Second argument missing"
+        return 2
+    fi
+
+    splitted_path1=(${1//// })
+    splitted_path2=(${2//// })
+    
+    count=0
+    has_current1=false
+    for i in "${!splitted_path1[@]}"; do
+        if [[ ${splitted_path1[i]} == "." ]]; then
+            ((count=count+1))
+            has_current1=true
+        fi
+    done
+    if [ $count -gt 1 ]; then
+        echo "resolve_path: [Error] First argument has more than 1 current directory symbols ('.')"
+        return 3
+    fi
+
+    count=0
+    has_current2=false
+    for i in "${!splitted_path2[@]}"; do
+        if [[ ${splitted_path2[i]} == "." ]]; then
+            ((count=count+1))
+            has_current2=true
+        fi
+    done
+    if [ $count -gt 1 ]; then
+        echo "resolve_path: [Error] Second argument has more than 1 current directory symbols ('.')"
+        return 4
+    fi
+    # echo "ddd $2"
+
+    resolved_path="."
+    for i in "${!splitted_path1[@]}"; do
+        resolved_path="$resolved_path/${splitted_path1[i]}"
+    done
+    for i in "${!splitted_path2[@]}"; do
+        resolved_path="$resolved_path/${splitted_path2[i]}"
+    done
+
+    echo "$resolved_path" | sed 's/^\.\///'
 }
 
 
 
 printf "" > $OUTPUT_FN
 
-resolve_path "co" "tam?"
+resolve_path "section1.2/exercise1.05/README.md" "section1.2/exercise1.05/README.md"
 
 find $DOCS_PATH -print0 | sort -z | while IFS= read -r -d '' file
 do
