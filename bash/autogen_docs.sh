c9a3e54f029bde96d3b060fcc53bbb8b919093dd
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-05-05 20:16:50 +0200

    Create parsing path function

--- bash/autogen_docs.sh
+++ bash/autogen_docs.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+# Global constants and variables
+DOCS_PATH="test/autogen_docs/chapter01/"
+DOCS_PATTERN=$(echo "$DOCS_PATH" | sed 's/\//\\\//g')
 OUTPUT_FN=output.md
-title=""
 EX_PATTERN="[Ee]xercise ?[0-9]+\.[0-9]+"
 EX_README_PATTERN="$EX_PATTERN/README.md$"
+title=""
+
+parse_path () {
+    echo "$1" | sed "s/$DOCS_PATTERN//"
+}
+
+resolve_path () {
+    PATTERN="chapter01\/(.*)"
+    echo "$1 $2"
+}
+
+
 
 printf "" > $OUTPUT_FN
 
-find test/autogen_docs/chapter01 -print0 | sort -z | while IFS= read -r -d '' file
+resolve_path "co" "tam?"
+
+find $DOCS_PATH -print0 | sort -z | while IFS= read -r -d '' file
 do
-    echo "$file"
+    # echo "$file"
+    parse_path $file
     if [ ! -d $file ]; then
         if [[ $file =~ $EX_README_PATTERN ]] && [[ $title != $(sed '1q;d' $file) ]]; then
             title=$(sed '1q;d' $file)
